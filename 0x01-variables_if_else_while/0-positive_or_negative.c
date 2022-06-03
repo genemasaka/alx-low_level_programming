fbccad743f6040bfaeaf69093c2b9e9e8506b5ff
genemasaka <masakagene@gmail.com>
2022-06-03 11:21:00 -0700

    learning control flow

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,9 @@
 
 
 
-/* betty style doc for function main goes there */
+/* function that genarates random numbers and prints whether
+ *it is positive, zoro or negative
+ * */
 
 int main(void)
 
@@ -23,12 +25,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 /* your code goes there */
-if (n>0) {
+if (n > 0) 
+{
 	printf("%d is positive\n", n);
-} else if(n==0) {
+} else if(n == 0) 
+{
 	printf("%d is zero\n", n);
-} else if(n<0) {
-	printf("%d ia negative\n", n);
+} else if(n<0) 
+{
+	printf("%d is negative\n", n);
 }
 
 return (0);
