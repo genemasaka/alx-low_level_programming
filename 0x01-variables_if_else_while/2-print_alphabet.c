79412a289a8b0598eae7677d3c9d5ec7185aa886
genemasaka <masakagene@gmail.com>
2022-06-04 13:36:33 -0700

    Prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+/**
+*main - prints the alphabet
+*in lowercase
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+char c = 'a';
+
+for (c = 'a'; c <= 'z'; ++c)
+{
+	putchar(c);
+}
+
+return (0);
+
+}
