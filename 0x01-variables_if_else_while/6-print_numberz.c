b4ff12fe3f88d96350105344ca8c1723a891c4f9
genemasaka <masakagene@gmail.com>
2022-06-06 11:19:19 -0700

    print numbers uzing print char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - function that prints numbers 
+*using putchar()
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+int putchar(int n);
+}
+putchar('\n');
+
+return (0);
+}
