5319d16a2736eb34d37a814a1b2de702fb1741c9
genemasaka <masakagene@gmail.com>
2022-06-17 09:23:35 -0700

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *main - prints 0 to 9
+ *Return: Always 0
+ */
+int main(void)
+{
+print_numbers();
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+int n;
+n = 0;
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+}
