739c6cfd7e5283b62f32673e9da2f241f778c5df
genemasaka <masakagene@gmail.com>
2022-06-17 07:46:29 -0700

    checks if letter is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isupper - determines whether letter is uppercase
+ *@c: int
+ *Return: Always 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A'' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#More functions and nested loops
+Learning more about functions and nested loops
