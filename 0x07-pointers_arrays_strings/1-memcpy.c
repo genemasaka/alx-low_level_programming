b3921b21fe32d218cf6ff79ffbeab536d6bb59c5
genemasaka <masakagene@gmail.com>
2022-06-19 10:22:59 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memcpy - copies memory area
+ *@dest: constant to be copied
+ *@src: source of data
+ *@n: bytes of memory
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+i = 0;
+while (i < n)
+{
+*(dest + i) = *(src + i);
+i++;
+}
+return (dest);
+}
