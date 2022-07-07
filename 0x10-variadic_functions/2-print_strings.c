efc9fb841a3a9ada4deb582894fe43f04c0138ba
genemasaka <masakagene@gmail.com>
2022-07-07 12:10:12 -0700

    variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,7 +8,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list valist;
-unsiggned int i;
+unsigned int i;
 char *str;
 va_start(valist, n);
 for (i = 0; i < n; i++)
