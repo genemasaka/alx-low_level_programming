71acdabb904ce4e33abbac5c6938dc5ef8df3060
genemasaka <masakagene@gmail.com>
2022-07-07 10:58:43 -0700

    function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "funtion_pointers.h"
+/**
+ * int_index - searches for an int
+ * @array: array to look through
+ * @size: size of the array
+ * @cmp: pointer to comparing function
+ * Return: return index of first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
+}
+}
+return (-1);
+}
